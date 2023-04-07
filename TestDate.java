a74a09980c6e731ad5009cbc58162e83ddc79674
Gascor <lucas-dasilvaferreira@hotmail.com>
2023-04-07 10:52:55 +0200

    Implémentation du Test sur Date.compareTo (Non finalisé)

--- TestDate.java
+++ TestDate.java
@@ -1,7 +1,28 @@
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
 public class TestDate {
+    @Parameter
+    public int input1;
+
+    @Parameterized.Parameters
+    public static final List<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                { 0 }, { 1 }, { 2 }, { 3 }, { 4 }, { 5 }, { 6 }, { 7 }, { 8 }, { 9 }, { 10 }
+        });
+    }
+
+    @Test
     public void test() {
-        assertEquals(input1, input2);
+        equals(input1);
     }
 }
